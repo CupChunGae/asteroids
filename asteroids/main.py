4cbd015227520f78bf3d3a5f5853d00e68bb960c
CupChunGae <c_nim@hotmail.com>
2025-11-20 14:19:30 +0000

    added wasd movement to the player

--- asteroids/main.py
+++ asteroids/main.py
@@ -13,17 +13,18 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     in_game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
         in_game_clock.tick(60)
-        dt = in_game_clock.tick()/1000
+        dt = in_game_clock.tick(60)/1000
         #print(f"{dt}")
        
 
